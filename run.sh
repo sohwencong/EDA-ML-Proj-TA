cfea47716d39563113bf28bba18fb79d85e33a38
sohwencong <122419514+sohwencong@users.noreply.github.com>
2023-06-05 14:38:54 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-mkdir data
-wget https://techassessment.blob.core.windows.net/aiap14-assessment-data/fishing.db -O ./data/fishing.db
+# mkdir data
+# wget https://techassessment.blob.core.windows.net/aiap14-assessment-data/fishing.db -O ./data/fishing.db
 
 python ./src/data_ingestion.py
